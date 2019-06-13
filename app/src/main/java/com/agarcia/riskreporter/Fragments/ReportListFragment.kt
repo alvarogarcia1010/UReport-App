e70c67d91541ed6eb2d04ad1ff896297aa8d43a8
Dalcoleas <00048015@uca.edu.sv>
2019-06-12 23:36:27 -0600

    Adapter and Recycler View

--- app/src/main/java/com/agarcia/riskreporter/Fragments/ReportListFragment.kt
+++ app/src/main/java/com/agarcia/riskreporter/Fragments/ReportListFragment.kt
@@ -7,46 +7,60 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
 
 import com.agarcia.riskreporter.R
+import com.agarcia.riskreporter.Report
+import com.agarcia.riskreporter.ReportAdapter
 import kotlinx.android.synthetic.main.fragment_report_list.*
+import kotlinx.android.synthetic.main.fragment_report_list.view.*
 
 class ReportListFragment : Fragment() {
+
+    var report = Report("Panal de Abejas","Juan Castro","A-33","20/10/2019")
+    var report1 = Report("Gotera en Cielo Falso" , "Karla Acevedo", "B-13", "09/04/19")
+
+    val mem : ArrayList<Report> = ArrayList()
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
     {
         val view = inflater.inflate(R.layout.fragment_report_list, container, false)
 
+        init(view)
+
         return view
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    fun init(view:View){
+        var adapter = object : ReportAdapter(view.context){
+            override fun setClickListenerToReport(holder: ViewHolder, item: Report) {
+                holder.itemView.setOnClickListener {
+                    val nextAction = ReportListFragmentDirections.nextAction()
+                    Navigation.findNavController(it).navigate(nextAction)
+                }
+            }
+        }
 
-        val fab: View = view.findViewById(R.id.fab)
-        val btnDetails: View = view.findViewById(R.id.list_btn_next_action)
-        val btnDetails1: View = view.findViewById(R.id.list_btn_next_action1)
-        val btnDetails2: View = view.findViewById(R.id.list_btn_next_action2)
+        val recyclerView = view.recyclerview
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(view.context)
+
+        val fab : View = view.findViewById(R.id.fab)
 
         fab.setOnClickListener { view ->
             Navigation.findNavController(view).navigate(R.id.new_action)
         }
 
-        btnDetails.setOnClickListener {
-            val nextAction = ReportListFragmentDirections.nextAction()
-            Navigation.findNavController(it).navigate(nextAction)
-        }
+        addReports()
 
-        btnDetails1.setOnClickListener {
-            val nextAction = ReportListFragmentDirections.nextAction()
-            Navigation.findNavController(it).navigate(nextAction)
-        }
+        adapter.changeDataSet(mem)
 
-        btnDetails2.setOnClickListener {
-            val nextAction = ReportListFragmentDirections.nextAction()
-            Navigation.findNavController(it).navigate(nextAction)
-        }
     }
 
+    fun addReports(){
+        mem.add(report)
+        mem.add(report1)
+    }
 
 }
