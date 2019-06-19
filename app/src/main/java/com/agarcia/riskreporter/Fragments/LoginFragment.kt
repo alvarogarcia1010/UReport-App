b420b3023c01d777333f237de2693f97d00f4c2d
Dalcoleas <00048015@uca.edu.sv>
2019-06-19 10:57:36 -0600

    Validaciones login, a espera de entidad Usuario para implementar Firebase

--- app/src/main/java/com/agarcia/riskreporter/Fragments/LoginFragment.kt
+++ app/src/main/java/com/agarcia/riskreporter/Fragments/LoginFragment.kt
@@ -12,10 +12,15 @@ import com.agarcia.riskreporter.R
 import kotlinx.android.synthetic.main.fragment_login.*
 import com.agarcia.riskreporter.Activities.MainActivity
 import android.content.Intent
-
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 
 class LoginFragment : Fragment() {
+
+
+    //private lateinit var auth : FirebaseAuth
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
     {
@@ -26,14 +31,56 @@ class LoginFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         login_btn_login.setOnClickListener {
-            val mIntent = Intent(activity, MainActivity::class.java)
-            startActivity(mIntent)
+            login()
         }
 
         login_btn_register.setOnClickListener {
             val nextAction = LoginFragmentDirections.nextAction()
             Navigation.findNavController(it).navigate(nextAction)
         }
+
     }
+
+    private fun login(){
+        if(!validate()){
+            failedLogin()
+            return
+        }
+
+        /*auth.signInWithEmailAndPassword(login_et_email.text.toString(),login_et_password.text.toString())
+            .addOnCompleteListener {
+                if(!it.isSuccessful) return@addOnCompleteListener
+                Log.d("Login", "Loggeado correctamente: ${it.result?.user?.uid}")
+                val mIntent = Intent(activity, MainActivity::class.java)
+                mIntent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(mIntent)
+            }*/
+
+    }
+
+    private fun validate():Boolean{
+        var valid = true
+
+        if(login_et_email.text.toString().isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(login_et_email.text.toString()).matches()){
+            login_et_email.error = "Ingrese un correo válido"
+            valid = false
+        } else{
+            login_et_email.error = null
+        }
+
+        if(login_et_password.text.toString().isEmpty() || login_et_password.text.toString().length < 8){
+            login_et_password.error = "Contraseña tiene que tener 8 cáracteres como minimo"
+            valid = false
+        } else{
+            login_et_password.error = null
+        }
+        return valid
+    }
+
+    private fun failedLogin(){
+        Toast.makeText(view?.context,"Login fallido", Toast.LENGTH_SHORT).show()
+        login_btn_login.isEnabled = true
+    }
+
 
 }
