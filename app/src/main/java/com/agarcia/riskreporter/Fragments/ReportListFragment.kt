a1467ba0304553acd190e690282cf1d2fffbb75c
alvarogarcia1010 <00111317@uca.edu.sv>
2019-06-21 16:22:28 -0600

    Implementando mapa

--- app/src/main/java/com/agarcia/riskreporter/Fragments/ReportListFragment.kt
+++ app/src/main/java/com/agarcia/riskreporter/Fragments/ReportListFragment.kt
@@ -1,11 +1,14 @@
 package com.agarcia.riskreporter.Fragments
 
 
+import android.app.Activity
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.Navigation
@@ -35,6 +38,12 @@ class ReportListFragment : Fragment() {
 
     fun init(view:View){
 
+        if(ActivityCompat.checkSelfPermission(activity as Activity,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(activity as Activity,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+        }
+
         reportViewModel = ViewModelProviders.of(this).get(ReportViewModel::class.java)
         var adapter = object : ReportAdapter(view.context){
             override fun setClickListenerToReport(holder: ViewHolder, item: Report) {
@@ -61,6 +70,10 @@ class ReportListFragment : Fragment() {
         reportViewModel.allReports.observe(this, Observer { reports ->
             reports?.let{ adapter.changeDataSet(it)}
         })
+    }
+
+    companion object{
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
     }
 
 }
